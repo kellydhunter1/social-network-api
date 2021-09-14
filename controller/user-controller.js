b34d528d160fd5c330255939be3d1b840d83556b
kellydhunter1 <kellydhunter1@gmail.com>
2021-09-14 15:32:17 -0400

    added routes for user and set up server

---
+++ controller/user-controller.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+
+const userController = {
+
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({_id: -1})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        })
+    },
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    }
+}
---
+++ routes/api/user-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const { getAllUsers, createUser } = require('../../controllers/user-controller');
+
+
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
